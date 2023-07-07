43c6c2dcd9546539a521b6fc5f0e09534839d7f5
Tek N. Chaudhary <tekchau21@gmail.com>
2023-07-07 18:16:03 +0530

    modified:   src/index.html 	modified:   src/script.js 	modified:   src/style.css

--- src/script.js
+++ src/script.js
@@ -1,27 +1,26 @@
 window.addEventListener('scroll', () => {
-    let value = window.scrollY;
-    
-    let text = document.getElementById('text');
-    let leaf = document.getElementById('leaf');
-    let plant = document.getElementById('plant');
-    let tree = document.getElementById('tree');
-    let hill1 = document.getElementById('hill1');
-    let hill2 = document.getElementById('hill2');
-    let hill3 = document.getElementById('hill3');
-    let hill4 = document.getElementById('hill4');
-    let hill5 = document.getElementById('hill5');
-    
-    if (text && leaf && hill1 && hill4 && hill5) {
-      text.style.marginTop = value * 2.5 + 'px';
-      leaf.style.top = value * -1.5 + 'px';
-      leaf.style.left = value * 1.5 + 'px';
-      hill5.style.left = value * 1.5 + 'px';
-      hill4.style.left = value * -1.5 + 'px';
-      hill1.style.top = value * 1 + 'px';
-    }
-    var elements = ["text", "leaf", "plant", "tree", "hill1", "hill2", "hill3", "hill4", "hill5"];
+  let value = window.scrollY;
 
-    if (value > 1000) {
+  let text = document.getElementById('text');
+  let leaf = document.getElementById('leaf');
+  let hill1 = document.getElementById('hill1');
+  let hill4 = document.getElementById('hill4');
+  let hill5 = document.getElementById('hill5');
+
+  if (text && leaf && hill1 && hill4 && hill5) {
+    text.style.marginTop = value * 2.5 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.top = value * 1 + 'px';
+    
+  }
+
+  var elements = ["text", "leaf","hill1", "hill2", "hill3", "hill4", "hill5"];
+
+    if (value > 600) {
+      // alert("reached");
       for (var i = 0; i < elements.length; i++) {
         var element = document.getElementById(elements[i]);
         element.style.visibility = "hidden";
@@ -32,5 +31,4 @@ window.addEventListener('scroll', () => {
         element.style.visibility = "visible";
       }
     }
-    
-  });
+});
