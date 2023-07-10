f312000fcee0049a7019657b118acf46915e51b1
Tek N. Chaudhary <tekchau21@gmail.com>
2023-07-10 11:49:17 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -56,19 +56,4 @@ document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("scroll", scrollFunction);
 });
 
-//  // Check device width on page load
-// checkDeviceWidth();
-//  // Check device width on window resize
-// window.addEventListener('resize', checkDeviceWidth);
-
-// function checkDeviceWidth() {
-//   var video = document.getElementById('video');
-//   var deviceWidth = window.innerWidth;
-
-//   if (deviceWidth <= 768) {
-//     video.setAttribute('controls', '');
-//   } else {
-//     video.removeAttribute('controls');
-//   }
-// }
 
